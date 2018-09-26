27087a57948c510aaf8f5ac9bc04cef6dc7187d9
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-09-26 16:12:13 +0100

    initial commit

---
+++ app/src/main/java/com/r4sh33d/iblood/LandingFragment.java
@@ -0,0 +1,47 @@
+package com.r4sh33d.iblood;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LandingFragment extends Fragment {
+
+
+    public LandingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_landing, container, false);
+        ButterKnife.bind(this , view);
+        return view;
+    }
+
+
+    @OnClick(R.id.blood_banks_button)
+    public void onBloodBankButtonClicked(){}
+
+
+    @OnClick(R.id.blood_seeker_button)
+    public void onBloodSeekerButtonClicked(){}
+
+
+    @OnClick(R.id.blood_donor_button)
+    public void onBloodDonorButtonClicked(){}
+
+
+}
---
+++ app/src/main/java/com/r4sh33d/iblood/LoginFragment.java
@@ -0,0 +1,41 @@
+package com.r4sh33d.iblood;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+    @OnClick(R.id.click_here_to_register_textview)
+    public void registerTextViewClicked () {
+        getFragmentManager().beginTransaction().replace(R.id)
+    }
+
+}
