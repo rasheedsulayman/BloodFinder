09c97e36fdf53996b1d14a7024f585c501248a2d
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-10-29 02:59:58 +0100

    Add BloodPosting Recycler Adapter

--- app/src/main/java/com/r4sh33d/iblood/postingsresultslist/PostingsListAdapter.java
+++ app/src/main/java/com/r4sh33d/iblood/postingsresultslist/PostingsListAdapter.java
@@ -2,31 +2,43 @@ package com.r4sh33d.iblood.postingsresultslist;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.r4sh33d.iblood.R;
 import com.r4sh33d.iblood.models.BloodPostingData;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PostingsListAdapter  extends RecyclerView.Adapter<PostingsListAdapter.PostingsHolder>{
+public class PostingsListAdapter extends RecyclerView.Adapter<PostingsListAdapter.PostingsHolder> {
+    private OnBloodPostingItemClickListener onBloodPostingItemClickListener;
     ArrayList<BloodPostingData> bloodPostingDataArrayList;
 
-    public PostingsListAdapter(ArrayList<BloodPostingData> bloodPostingData) {
+    public PostingsListAdapter(OnBloodPostingItemClickListener onBloodPostingItemClickListener ,
+                               ArrayList<BloodPostingData> bloodPostingData) {
+        this.onBloodPostingItemClickListener = onBloodPostingItemClickListener;
         this.bloodPostingDataArrayList = bloodPostingData;
     }
 
     @NonNull
     @Override
     public PostingsHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_blood_posting,
+                viewGroup, false);
+        return new PostingsHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PostingsHolder postingsHolder, int i) {
-
+         BloodPostingData bloodPostingData = bloodPostingDataArrayList.get(i);
+         postingsHolder.donorsBloodTypeTextView.setText(bloodPostingData.donorsBloodType);
+         postingsHolder.donorLocationNameTextView.setText(bloodPostingData.donorsName);
+         //TODO comeback and set address
     }
 
     @Override
@@ -34,14 +46,27 @@ public class PostingsListAdapter  extends RecyclerView.Adapter<PostingsListAdapt
         return bloodPostingDataArrayList.size();
     }
 
-    class PostingsHolder extends RecyclerView.ViewHolder {
+    class PostingsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.donor_name_textview)
+        TextView donorNameTextView;
+        @BindView(R.id.location_name_textview)
+        TextView donorLocationNameTextView;
+        @BindView(R.id.blood_type_textview)
+        TextView donorsBloodTypeTextView;
 
         public PostingsHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
-            
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onBloodPostingItemClickListener.onBloodPostingItemClicked(bloodPostingDataArrayList.get(getAdapterPosition()));
         }
     }
 
-
+    interface OnBloodPostingItemClickListener{
+        void onBloodPostingItemClicked(BloodPostingData bloodPostingData);
+    }
 }
